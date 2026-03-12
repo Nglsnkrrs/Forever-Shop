90c431216c32344a6c7212bb5ab1416ebdb71109
Nglsnkrrs <ternavsdop@gmail.com>
2026-03-12 17:00:00 +0300

    first commit

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { ShopContext } from '../context/shopContext'
+import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
 import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
